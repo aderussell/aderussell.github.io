dbdf1ec43d72d0b2f9c95fb8c150c94f68911fe5
aderussell <adrianrussell@me.com>
2017-06-28 20:09:50 +0100

    added code for bilinear texturing (turned off)

--- demos/opengl - tutorial 0 - example 0/Core/texture.js
+++ demos/opengl - tutorial 0 - example 0/Core/texture.js
@@ -41,15 +41,51 @@ class Texture {
 	
 		x = Math.floor(x);
 		y = Math.floor(y);
+		//x1 = x+1;
+		//y1 = y+1;
 		
 		return this.imageData[x + (y*this.width)];
-	
-// 		var pixelDataR = this.colorBuffer[((x + (y*this.canvas.width)) * 4)+0];
-// 		var pixelDataG = this.colorBuffer[((x + (y*this.canvas.width)) * 4)+1];
-// 		var pixelDataB = this.colorBuffer[((x + (y*this.canvas.width)) * 4)+2];
-// 		var color = new Color(pixelDataR/255.0, pixelDataG/255.0, pixelDataB/255.0);
-// 		return color;
 	}
+	
+// 	getColor(x, y) {		
+// 		return this.bilinear(x, y);
+// 	}
+	
+	bilinear(x, y) {
+	
+	  var x = x * this.width;
+	  var y = (1.0-y) * this.height;
+	
+	  var percentX = x - Math.floor(x);
+      var percentY = y - Math.floor(y);
+	
+	  x = Math.floor(x);
+	  y = Math.floor(y);
+	  var x1 = x+1;
+	  var y1 = y+1;
+	
+      
+      
+      var a = this.imageData[x + (y*this.width)];
+      var b = this.imageData[x + (y1*this.width)];
+      var c = this.imageData[x1 + (y*this.width)];
+      var d = this.imageData[x1 + (y1*this.width)];
+      
+  
+      var topR = (b.r * percentX) + (d.r * (1.0 - percentX));
+      var bottomR = (a.r * percentX) + (c.r * (1.0 - percentX));
+      var finalR = topR * percentY + bottomR * (1.0 - percentY);
+  
+  	  var topG = (b.g * percentX) + (d.g * (1.0 - percentX));
+      var bottomG = (a.g * percentX) + (c.g * (1.0 - percentX));
+      var finalG = topG * percentY + bottomG * (1.0 - percentY);
+      
+      var topB = (b.b * percentX) + (d.b * (1.0 - percentX));
+      var bottomB = (a.b * percentX) + (c.b * (1.0 - percentX));
+      var finalB = topB * percentY + bottomB * (1.0 - percentY);
+  
+      return new Color(finalR, finalG, finalB);
+  }
 }
 
 
